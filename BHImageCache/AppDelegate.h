b1ba94632846f66a8e40bce46a3443216b98596d
iPhonig <bhonig@iphonig.com>
2014-09-10 13:57:26 -0400

    Initial Commit

---
+++ BHImageCache/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BHImageCache
+//
+//  Created by Ben Honig on 9/10/14.
+//  Copyright (c) 2014 iPhonig, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BHImageCache/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BHImageCache
+//
+//  Created by Ben Honig on 9/10/14.
+//  Copyright (c) 2014 iPhonig, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
